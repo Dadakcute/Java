f86fec058c102700f0cb313f7f3587bb12e5d4fd
Dadakcute <isackcoelho731@gmail.com>
2026-03-23 15:27:04 -0300

    Criar funcao1bom.java

---
+++ funcao1bom.java
@@ -0,0 +1,20 @@
+import java.time.LocalTime;
+
+public class Main {
+
+    public static void saudacao(String nome) {
+        int hora = LocalTime.now().getHour();
+
+        if (hora < 12) {
+            System.out.println("Bom dia, " + nome);
+        } else if (hora < 18) {
+            System.out.println("Boa tarde, " + nome);
+        } else {
+            System.out.println("Boa noite, " + nome);
+        }
+    }
+
+    public static void main(String[] args) {
+        saudacao("Zack");
+    }
+}
