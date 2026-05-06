721e21db134e83785122d6c545985d713e9657a0
Dadakcute <isackcoelho731@gmail.com>
2026-05-06 08:25:06 -0300

    Atualizar o Pessoa.java

--- Test.java
+++ Test.java
@@ -1,43 +1,14 @@
-class aluno {
+class Pessoa {
 
-    private String nome;
-    private int matricula;
-    private String dtNascimento;
-    private String telefone;
-    private String nomeResponsavel;
-
-    public aluno(String nome, int matricula) {
-        this.nome = nome;
-        this.matricula = matricula;
+    public void saudacao(Pessoa pessoa) {
+        return "Olá, pessoa!";
     }
 
-    public String apresentar() {
-        return "Olá eu sou " + this.nome +
-               ", nasc: " + dtNascimento +
-               ", tel: " + telefone +
-               ", resp: " + nomeResponsavel;
+    public void saudacao(Aluno aluno) {
+        return "Olá, aluno!";
     }
 
-    public String saudacao() {
-        int hora = java.time.LocalTime.now().getHour();
-
-        if (hora < 12) {
-            return "Bom dia " + nome;
-        } else if (hora < 18) {
-            return "Boa tarde " + nome;
-        } else {
-            return "Boa noite " + nome;
-        }
-    }
-}
-
-public class Test {
-
-    public static void main(String[] args) {
-
-        aluno aluno = new aluno("Isack", 6);
-
-        System.out.println(aluno.apresentar());
-        System.out.println(aluno.saudacao());
+    public void saudacao(Professor professor) {
+        return "Bom dia, professor!"
     }
 }
