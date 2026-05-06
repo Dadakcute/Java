521322bfe389c42b36671548465c74c8271b97f8
Dadakcute <isackcoelho731@gmail.com>
2026-05-06 08:26:07 -0300

    Criar Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,14 @@
+class Pessoa {
+
+    public void saudacao(Pessoa pessoa) {
+        return "Olá, pessoa!";
+    }
+
+    public void saudacao(Aluno aluno) {
+        return "Olá, aluno!";
+    }
+
+    public void saudacao(Professor professor) {
+        return "Bom dia, professor!"
+    }
+}
