12b6aeb86b07b0991507ee2dab471786e1549875
Dadakcute <isackcoelho731@gmail.com>
2026-05-06 07:36:57 -0300

    Atualizar o Pessoa.java

--- encapsulamento.java
+++ encapsulamento.java
@@ -14,34 +14,3 @@ class Pessoa {
     }
 }
 
-class Aluno extends Pessoa {
-    @Override
-    public void saudacao() {
-        System.out.println("Oi, professor!");
-    }
-}
-
-class Professor extends Pessoa {
-    @Override
-    public void saudacao() {
-        System.out.println("Bom dia, aluno
-!");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-
-        Pessoa p1 = new Aluno();
-        Pessoa p2 = new Professor();
-
-        p1.setNome("Carlos");
-        p2.setNome("Ana");
-
-        System.out.println(p1.getNome());
-        p1.saudacao();
-
-        System.out.println(p2.getNome());
-        p2.saudacao();
-    }
-}
